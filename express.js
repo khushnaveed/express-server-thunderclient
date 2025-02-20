084d267482002d15f63ebc93b140c14bf8f37320
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-02-20 21:04:06 +0100

    add middleware

--- express.js
+++ express.js
@@ -6,6 +6,16 @@ const PORT = 8000;
 
 //path along with the method is called endpoint
 
+//middleware - a function receives 3 parameters and it can send back response or forward your request to next handler
+app.use((req, res, next)=>{
+   //res.send("i accept all method requests")
+   console.log("method", req.method, "path", req.url);
+   if(req.method === "PUT"){
+    res.send("not accceptable")
+   }
+   next();
+})
+
 //users endpoints
 app.get("/users", (req, res) => {
   res.send("recieved your GET request on /users");
@@ -22,6 +32,10 @@ app.patch("/users", (req, res) => {
 app.delete("/users", (req, res) => {
   res.send("recieved your DELETE request on /users");
 });
+
+app.use((req, res)=>{
+  res.send("no such req")
+})
 
 //products endpoints
 app.get("/products", (req, res) => {
@@ -43,7 +57,7 @@ app.delete("/products", (req, res) => {
 //orders endpoints
 app.get("/orders", (req, res) => {
   res.send("recieved your GET request on /orders");
-});
+}); 
 
 app.post("/orders", (req, res) => {
   res.send("recieved your POST request on /orders");
